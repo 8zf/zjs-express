9650ef2c535aadb23dc33279c0e643200372b348
ZhangFeng <zhangfeng2333@gmail.com>
2017-02-22 20:45:27 +0800

    add new function to calculate colors

--- app.js
+++ app.js
@@ -40,12 +40,19 @@ app.post('/send', function (req, res) {
         datas[0] = datas[0].substring(1);
 
         var result = [];
+        var result2 = [];
 
         for (i = 0; i < datas.length; i++)
         {
             result[i] = '';
+            result2[i] = '';
             if (datas[i].length < 2)
                 continue;
+
+            //计算result2
+            result2[i] = (countSemicolon(datas[i])+1)*24;
+
+            //将每一行以分号分割成数组
             datas[i] = datas[i].split(";");
             //对每一行，行中会有多个数据
             for (j = 0; j < datas[i].length; j++)
@@ -58,14 +65,22 @@ app.post('/send', function (req, res) {
             // console.log(result[i]);
         }
 
-        if (fs.existsSync(path.join(__dirname, 'uploads/result' ,(files.content[0].originalFilename))) == true)
+        //1
+        if (fs.existsSync(path.join(__dirname, 'uploads/result' ,files.content[0].originalFilename)) == true)
         {
-            fs.unlinkSync(path.join(__dirname, 'uploads/result' ,(files.content[0].originalFilename)));
+            fs.unlinkSync(path.join(__dirname, 'uploads/result' ,files.content[0].originalFilename));
+        }
+
+        //2
+        if (fs.existsSync(path.join(__dirname, 'uploads/result2' ,files.content[0].originalFilename)) == true)
+        {
+            fs.unlinkSync(path.join(__dirname, 'uploads/result2' ,files.content[0].originalFilename));
         }
 
         for (k = 0; k < result.length; k++)
         {
             fs.appendFileSync(path.join(__dirname, 'uploads/result' ,(files.content[0].originalFilename)), result[k] + "\n");
+            fs.appendFileSync(path.join(__dirname, 'uploads/result2' ,(files.content[0].originalFilename)), result2[k] + "\n");
         }
 
         var options = {
@@ -106,4 +121,14 @@ function genConv(x) {
         "222:3::" + x + ";20549:28393;" +
         "222:3::" + x + ";20549:28394;" +
         "222:3::" + x + ";20549:28395;";
+}
+
+function countSemicolon(strSource) {
+    //统计字符串中包含{}或{xxXX}的个数
+    var thisCount = 0;
+    strSource.replace(/;/g, function (m, i) {
+        //m为找到的{xx}元素、i为索引
+        thisCount++;
+    });
+    return thisCount;
 }
